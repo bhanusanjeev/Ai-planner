74f909a5fe7cf53d6c10694f977e49681d0b58d8
bhanusanjeev <bhanusanjeev19@gmail.com>
2025-12-14 16:59:27 +0530

    updates hosts in settings.py

--- Aiplanner/Aiplanner/settings.py
+++ Aiplanner/Aiplanner/settings.py
@@ -33,11 +33,12 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com"
+]
 
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
-).split(",")
 
 # Application definition
 
