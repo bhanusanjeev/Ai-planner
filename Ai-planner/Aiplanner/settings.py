bc53b95ea8c5da627f1088af1c7aab0787474b71
bhanusanjeev <bhanusanjeev19@gmail.com>
2025-12-16 03:23:24 +0530

    initial push after first deployment to track server 500 error

--- Ai-planner/Aiplanner/settings.py
+++ Ai-planner/Aiplanner/settings.py
@@ -32,11 +32,11 @@ SECRET_KEY = os.environ.get(
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
-    ".onrender.com"
+    "ai-planner-7aa2.onrender.com"
 ]
 
 
